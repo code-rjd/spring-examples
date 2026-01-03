98870bc91ee27809c05e7cfe823d114eb3b8721a
unknown <rjdeshmukh111@gmail.com>
2026-01-03 16:28:03 +0530

    changes added

--- spring-hello-world-annotation/src/main/java/com/coderjd/practice/App.java
+++ spring-hello-world-annotation/src/main/java/com/coderjd/practice/App.java
@@ -1,26 +1,18 @@
 package com.coderjd.practice;
 
+import com.coderjd.practice.config.AppConfig;
 import com.coderjd.practice.model.Address;
 import com.coderjd.practice.model.City;
 import com.coderjd.practice.service.HelloService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
-/**
- * Hello world!
- *
- */
-
-@Configuration
-@ComponentScan(basePackages = "com.coderjd.practice")
 public class App {
     public static ApplicationContext context;
     public static void main( String[] args ) {
         City city = City.builder().name("Pune").build();
         Address address = Address.builder().city(city).build();
-        context = new AnnotationConfigApplicationContext(App.class);
+        context = new AnnotationConfigApplicationContext(AppConfig.class);
         HelloService helloService= context.getBean("helloService", HelloService.class);
 
         helloService.setName("Amit");
---
+++ spring-hello-world-annotation/src/main/java/com/coderjd/practice/config/AppConfig.java
@@ -0,0 +1,9 @@
+package com.coderjd.practice.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.coderjd.practice")
+public class AppConfig {
+}
